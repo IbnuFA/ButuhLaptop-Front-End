43511624c93b133ee71c695ca721ea2f33552aa5
IbnuFA <60996611+IbnuFA@users.noreply.github.com>
2023-07-31 14:49:26 +0700

    add protected user(not activated)

--- src/page/user/UserDetailCheckout.jsx
+++ src/page/user/UserDetailCheckout.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getUserLogin } from "../../features/authSlice";
 
 import NavUser from "../../components/navBar/NavUser";
 import DetailCheckout from "../../components/user/DetailCheckout";
 
 export default function UserDetailCheckout(){
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const {isError} = useSelector((state => state.auth));
+
+    //cek user login
+    // useEffect(() => {
+    //     dispatch(getUserLogin())
+    // }, [dispatch]);
+
+    // useEffect(() => {
+    //     if(isError){
+    //         navigate("*");
+    //     }
+    // }, [isError, navigate])
+    
     return(
         <>
             <NavUser/>

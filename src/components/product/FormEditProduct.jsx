53660fad825f697bd05d4c1db17081234f799ec0
IbnuFA <60996611+IbnuFA@users.noreply.github.com>
2024-01-20 00:38:40 +0700

    add update product implementation

--- src/components/product/FormEditProduct.jsx
+++ src/components/product/FormEditProduct.jsx
@@ -1,10 +1,11 @@
 import React , {useState, useEffect} from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
+import Token from "../../features/token";
 
 import '../../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Row, Col, Form, Button, Card, Table} from "react-bootstrap";
+import {Row, Col, Form, Button, Card} from "react-bootstrap";
 
 //import icon
 import { GiCancel } from "react-icons/gi";
@@ -24,39 +25,45 @@ export default function FormEditProduct(){
     const [stock, setStock] = useState("");
     const [image, setImage] = useState("");
     const [description, setDescription] = useState("");
+    const [weight, setWeight] = useState("")
     const [msg, setMsg] = useState("");
 
-    useEffect(() => {
-        const getProductById = async () => {
-            try {
-                const response = await axios.get(
-                    `http://localhost:5000/products/${id}`
-                );
-                setName(response.data.name);
-                setPrice(response.data.price);
-                setCategory(response.data.category);
-                setStock(response.data.stock);
-                setImage(response.data.image);
-                setDescription(response.data.description);
-            } catch (error) {
-                if (error.response) {
-                    setMsg(error.response.data.msg);
-                }
+    const getProductById = async () => {
+        try {
+            const response = await axios.get(
+                `http://localhost:5000/products/${id}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${Token.getToken()}`
+                    }
+            });
+            
+            setProducts(response.data)
+        } catch (error) {
+            if (error.response) {
+                setMsg(error.response.data.msg);
             }
-        };
-        getProductById();
-      }, [id]);
+        }
+    };
 
-      const updateProduct = async(e) => {
+    const updateProduct = async(e) => {
         e.preventDefault();
         try {
-            await axios.patch(`http://localhost:5000/products/${id}`, {
-                name : name,
-                category: category,
-                price : price,
-                stock : stock,
-                image : image,
-                description : description
+            const form = new FormData()
+            form.append('name', name);
+            form.append('description', description);
+            form.append('category', category);
+            form.append('price', price);
+            form.append('stock', stock);
+            if (image[0]) {
+                form.append('image', image[0]);
+              }
+            form.append('weight', weight);
+
+            await axios.patch(`http://localhost:5000/admin/product/${id}`, form, {
+                headers: {
+                    Authorization: `Bearer ${Token.getToken()}`
+                }
             })
             navigate("/admin/listproduct")
         } catch (error) {
@@ -66,6 +73,10 @@ export default function FormEditProduct(){
         }
       }
 
+      useEffect(() => {
+        getProductById();
+      }, [id]);
+
       return(
         <>
             <Card className="col-md-11 col-sm-11 mx-auto mt-4">
@@ -74,13 +85,15 @@ export default function FormEditProduct(){
                     <Row>
                         <Col md={11} sm={12} className="mx-auto">
                             <div class="row align-items-center">
-                                <Form onSubmit={updateProduct}>
+                                
+                                    <Form onSubmit={updateProduct}>
                                     <Row className="mb-3">
                                         <Form.Group as={Col} md="8" controlId="exampleForm.ControlInput1">
                                             <Form.Label>Nama Produk</Form.Label>
                                             <Form.Control
                                                 type="text"
                                                 className="form-control input bg-light fs-6"
+                                                placeholder={products.name}
                                                 value={name}
                                                 onChange={(e) => setName(e.target.value)}
                                                 required
@@ -94,8 +107,9 @@ export default function FormEditProduct(){
                                                 className="form-control input bg-light"
                                                 value={category}
                                                 onChange={(e) => setCategory(e.target.value)}
+                                                required
                                             >
-                                                <option>Plilih Kategori</option>
+                                                <option>Ubah Kategori</option>
                                                 <option value="kerja">Kerja</option>
                                                 <option value="gaming">Gaming</option>
                                                 <option value="daily">Daily Use</option>
@@ -108,24 +122,25 @@ export default function FormEditProduct(){
                                             <Form.Label>Harga Produk</Form.Label>
                                             <Form.Control
                                                 type="text"
-                                                placeholder=""
                                                 className="form-control input bg-light fs-6"
+                                                placeholder={products.price}
                                                 value={price}
                                                 onChange={(e) => setPrice(e.target.value)}
                                                 required
                                         />
                                         </Form.Group>
                                         
-                                        {/* <Form.Group as={Col} md="4">
-                                            <Form.Label>Kategori Harga</Form.Label>
-                                            <Form.Select aria-label="Default select example" className="form-control bg-light">
-                                                <option>Plilih Kategori</option>
-                                                <option value="pas">Duit Pas</option>
-                                                <option value="modal_dikit">Ada Modal Dikit</option>
-                                                <option value="modal_banyak">Modal Banyak</option>
-                                                <option value="sultan">Sultan</option>    
-                                            </Form.Select>
-                                        </Form.Group> */}
+                                        <Form.Group as={Col} md="4">
+                                                <Form.Label>Berat Barang</Form.Label>
+                                                <Form.Control
+                                                    type="text"
+                                                    className="form-control input bg-light fs-6"
+                                                    placeholder={products.weight}
+                                                    value={weight}
+                                                    onChange={(e) => setWeight(e.target.value)}
+                                                    required
+                                            />
+                                        </Form.Group>
                                     </Row>
 
                                     <Row className="mb-3">
@@ -134,8 +149,7 @@ export default function FormEditProduct(){
                                             <Form.Control 
                                                 type="file" 
                                                 className="form-control input bg-light fs-6"
-                                                value={image}
-                                                onChange={(e) => setImage(e.target.value)}
+                                                onChange={(e) => setImage(e.target.files)}
                                             />
                                         </Form.Group>
                                         
@@ -143,10 +157,11 @@ export default function FormEditProduct(){
                                             <Form.Label>Stock Produk</Form.Label>
                                             <Form.Control
                                                 type="text"
-                                                placeholder=""
                                                 className="form-control input bg-light fs-6"
+                                                placeholder={products.stock}
                                                 value={stock}
                                                 onChange={(e) => setStock(e.target.value)}
+                                                required
                                             />
                                         </Form.Group>
                                     </Row>
@@ -155,7 +170,8 @@ export default function FormEditProduct(){
                                         <Form.Label>Deskripsi Produk</Form.Label>
                                         <Form.Control 
                                             as="textarea" 
-                                            className="form-control input bg-light fs-6" 
+                                            className="form-control input bg-light fs-6"
+                                            placeholder={products.description}
                                             rows={3}
                                             value={description}
                                             onChange={(e) => setDescription(e.target.value)}
@@ -174,6 +190,8 @@ export default function FormEditProduct(){
                                         </Col>
                                     </Row>                       
                                 </Form>
+                                
+                                
                             </div>
                         </Col>
                     </Row>

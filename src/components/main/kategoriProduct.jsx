e754e494be8d0c32c42d2299905a823fb989ed19
IbnuFA <60996611+IbnuFA@users.noreply.github.com>
2024-02-05 06:54:35 +0700

    change design category

--- src/components/main/kategoriProduct.jsx
+++ src/components/main/kategoriProduct.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Button, Card, Row, Col } from "react-bootstrap";
+import { Container, Button, Card, Row, Col, Image } from "react-bootstrap";
 
 //import gambar
 import Work from '../../asset/img/Work.png'
@@ -11,65 +11,67 @@ import Study from '../../asset/img/Study.png'
 
 export default function KategoriProduct (){
     return(
-        <Container className="mt-3 mb-3">
-            <h2 className="kategoriText">Cari yang Kamu Butuh!</h2>
-            <h5 className="kategoriText">Yuk, cari Produk yang kamu butuhkan disini</h5>
-            <Row className="justify-content-between">
-
-                <Col md={4} sm={6}>
-                    <Card className="col-sm-12 mb-4">
-                        <Card.Img variant="top" className="Custom-Category-Img"src={Work}/>
-                        <Card.Body>
-                            <Card.Title>Kerja</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-
-                            <Container className="d-flex justify-content-center">
-                                <Button className="mx-auto" variant="primary" size="lg">Cek Sekarang!</Button>
-                            </Container>
-                        </Card.Body>
-                    </Card>
-                </Col>
-
-                <Col md={4} sm={6}>
-                    <Card className="col-sm-12 mb-4">
-                        <Card.Img variant="top" className="Custom-Category-Img" src={Gaming} />
-                        <Card.Body>
-                            <Card.Title>Gaming</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-
-                            <Container className="d-flex justify-content-center">
-                                <Button className="mx-auto" variant="primary" size="lg">Cek Sekarang!</Button>
-                            </Container>
-                        </Card.Body>
-                    </Card>
-                </Col>
-
-                <Col md={4} sm={6}>
-                    <Card className="col-sm-12 mb-4">
-                        <Card.Img variant="top" className="Custom-Category-Img" src={Study} />
-                        <Card.Body>
-                            <Card.Title>Belajar</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-
-                            <Container className="d-flex justify-content-center">
-                                <Button className="mx-auto" variant="primary" size="lg">Cek Sekarang!</Button>
-                            </Container>
-                        </Card.Body>
-                    </Card>
-                </Col>
-
+        <>
+                <Row className="mt-4">
+                    <h3 className="kategoriText mt-3">Cari yang Kamu Butuh!</h3>
+                    <h5 className="kategoriText">Yuk, cari Produk yang kamu butuhkan disini</h5>
+                </Row>
+            <Container className="custom-category mt-3 mb-3" fluid>
                 
-            </Row>
-           
-        </Container>
+                <Row className="justify-content-between">
+                    <Col md={4} sm={6}>
+                        <div className="d-flex flex-column align-items-center mt-3 mb-3">
+                            <Image
+                                className="Custom-Category-Img border border-primary mb-2"
+                                src={Work}
+                                
+                                roundedCircle
+                                fluid
+                            />
+                            <h3>Kerja</h3>
+                            <p>
+                                Some quick example text to build on the card title and make up thebulk of the card's content.
+                            </p>
+                            <Button className="mx-auto" variant="primary" size="lg">Cek Sekarang!</Button>                          
+                        </div>
+
+                        
+                    </Col>
+
+                    <Col md={4} sm={6}>
+                        <div className="d-flex flex-column align-items-center mt-3 mb-3">
+                            <Image
+                                className="Custom-Category-Img border border-primary mb-2"
+                                src={Gaming}
+                                
+                                roundedCircle
+                                fluid
+                            />
+                            <h3>Gaming</h3>
+                            <p>
+                                Some quick example text to build on the card title and make up thebulk of the card's content.
+                            </p>
+                            <Button className="mx-auto" variant="primary" size="lg">Cek Sekarang!</Button>                          
+                        </div>
+                    </Col>
+                    <Col md={4} sm={6}>
+                        <div className="d-flex flex-column align-items-center mt-3 mb-3">
+                            <Image
+                                className="Custom-Category-Img border border-primary mb-2"
+                                src={Study}
+                                
+                                roundedCircle
+                                fluid
+                            />
+                            <h3>Belajar</h3>
+                            <p>
+                                Some quick example text to build on the card title and make up thebulk of the card's content.
+                            </p>
+                            <Button className="mx-auto" variant="primary" size="lg">Cek Sekarang!</Button>                          
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
--- src/page/tes-page.jsx
+++ src/page/tes-page.jsx
@@ -1,27 +1,19 @@
 import React from "react";
 
 
-import DetailKeranjang from "../components/user/DetailKeranjang";
-import FormRegister from "../components/FromRegister";
-import DetailCheckout from "../components/user/DetailCheckout";
 import CarouselProduk from "../components/main/carouselProduk";
+import CarouselMain from "../components/main/carouselMain";
+import KategoriProduct from "../components/main/kategoriProduct";
 
-
-import CatalogProduct from "../components/main/CatalogProduct";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function TesPage() {
     return(
         <>
-            {/* <CarouselMain className="mb-4"/>
-            <NavAdmin className="mb-4"/> */}
-            <FormRegister className="mb-4"/>
-            <CatalogProduct className="mb-4"/>
+            <KategoriProduct className="mb-4"/>
 
             <CarouselProduk className="mb-4"/>
-            <DetailKeranjang className="mb-4"/>
-            <DetailCheckout className="mb-4"/>
         </>
     )
 }
